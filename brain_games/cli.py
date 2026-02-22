4abee1a55b8f7d692dde2a3eb6ddec2b56b8401b
Ruslan Gilyazov <kazgrmruslan@yandex.ru>
2026-02-22 23:29:51 +0400

    Add user greeting with prompt library

---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+import prompt
+
+
+def welcome_user():
+    """Запрашивает имя пользователя и приветствует его."""
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
